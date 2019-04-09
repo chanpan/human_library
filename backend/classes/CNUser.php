eeee5ec7d0497da2ed81faab054c74112e4898a4
NuttaphonChanpan <chanpan.nuttaphon@gmail.com>
2019-04-09 14:07:41 +0700

    #fix

--- backend/classes/CNUser.php
+++ backend/classes/CNUser.php
@@ -1,47 +1,61 @@
 <?php
+
 namespace backend\classes;
+
 class CNUser {
-    public static function get_fullname_by_user_id($user_id){
+
+    public static function get_fullname_by_user_id($user_id) {
         $user = \common\models\User::findOne($user_id);
-        if($user){
-            $firstname = isset($user['firstname'])?$user['firstname']:'';
-            $lastname = isset($user['lastname'])?$user['lastname']:'';
+        if ($user) {
+            $firstname = isset($user['firstname']) ? $user['firstname'] : '';
+            $lastname = isset($user['lastname']) ? $user['lastname'] : '';
             return "{$firstname} {$lastname}";
         }
     }
-    public static function get_user_id(){
-        $id = isset(\Yii::$app->session['user_id'])?\Yii::$app->session['user_id']:'1';
+
+    public static function get_user_id() {
+        $id = isset(\Yii::$app->session['user_id']) ? \Yii::$app->session['user_id'] : '1';
         //\appxq\sdii\utils\VarDumper::dump($id);
         return $id;
         //return isset(\Yii::$app->user->id)?\Yii::$app->user->id:'';
     }
-    public static function can_admin(){
+
+    public static function can_admin() {
         $user_id = self::get_user_id();
         $user = \backend\models\User::findOne($user_id);
-        $role = \appxq\sdii\utils\SDUtility::string2Array($user->role);
-        if(in_array(1, $role)){
-            return true;
-        } 
+        if ($user) {
+            $role = \appxq\sdii\utils\SDUtility::string2Array($user->role);
+            if (in_array(1, $role)) {
+                return true;
+            }
+        }
         return false;
     }
-    public static function can_manager(){
+
+    public static function can_manager() {
         $user_id = self::get_user_id();
         $user = \backend\models\User::findOne($user_id);
-        $role = \appxq\sdii\utils\SDUtility::string2Array($user->role);
-        
-        if(in_array(2, $role)){
-            
-            return true;
-        } 
+        if ($user) {
+            $role = \appxq\sdii\utils\SDUtility::string2Array($user->role);
+
+            if (in_array(2, $role)) {
+
+                return true;
+            }
+        }
         return false;
     }
-    public static function can_member(){
+
+    public static function can_member() {
         $user_id = self::get_user_id();
         $user = \backend\models\User::findOne($user_id);
-        $role = \appxq\sdii\utils\SDUtility::string2Array($user->role);
-        if(in_array(3, $role)){
-            return true;
-        } 
+        if ($user) {
+            $role = \appxq\sdii\utils\SDUtility::string2Array($user->role);
+            if (in_array(3, $role)) {
+                return true;
+            }
+        }
         return false;
     }
+
 }
