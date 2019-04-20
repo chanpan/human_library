72c3dd87fe05751a49367931a3bdbc9a76e45cfa
NuttaphonChanpan <chanpan.nuttaphon@gmail.com>
2019-04-20 22:13:08 +0700

    fix

--- backend/views/book/_form.php
+++ backend/views/book/_form.php
@@ -17,6 +17,9 @@ use kartik\widgets\TimePicker;
             <?= $form->field($model, 'detail')->widget(dosamigos\ckeditor\CKEditor::className(), [
                     'options' => ['rows' => 6],
                     'preset' => 'full', //basic,standard,full  
+                    'clientOptions' => [
+                        'filebrowserUploadUrl' => \yii\helpers\Url::to(['/ck-editor/upload']),
+                    ]
                 ]) ?>
             <?= $form->field($model, 'user_name')->textInput(['maxlength' => true]) ?>
             
